745c4b3a9ace3e09c75d3853e9803b4ce8f9b995
kaizak <52350756+kaizak@users.noreply.github.com>
2019-06-30 20:33:48 +0700

    tambah no. 5

---
+++ 29juni/lima.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+var data = "5956560159466056".split("0"), aa = data[0], bb = data[1];
+var pisah1 = aa.split("");
+var pisah2 = bb.split("");
+urut1 = pisah1.sort(); 
+urut2 = pisah2.sort();
+gabung1 = urut1.join([separator = '']);
+gabung2 = urut2.join([separator = '']);
+document.write(gabung1 + gabung2);
+//alert(gabung1 + gabung2);
+</script>
